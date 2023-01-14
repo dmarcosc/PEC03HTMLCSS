f78827d9a1807aea28dd5c0291fa5b5a74f11b0c
dmarcosc <carcharoth92@gmail.com>
2023-01-14 19:20:42 +0100

    fixes and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      'primary-color': '#ffff',
+      'secondary-color': '#4e4e50',
+      'highlight-color': '#c3073f',
+      'bg-color': '#1a1a1d',
+    },
+    fontFamily: {
+      sansserif: ['sans-serif'],
+      helvetica: ['Helvetica Neue'],
+    },
     extend: {},
   },
   plugins: [],
